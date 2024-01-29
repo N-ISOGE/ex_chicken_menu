88f5284b0494753cee9bf0f76392d26c8a13698d
daseeni <daseeni@gmail.com>
2024-01-29 00:17:11 +0000

    nix 설정 반영

---
+++ .idx/dev.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }: {
+
+  # Which nixpkgs channel to use.
+  channel = "unstable"; # or "stable-23.05"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.corepack_21
+    pkgs.rustup
+    pkgs.rustfmt
+    pkgs.rust-analyzer
+    pkgs.clippy
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+
+  # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+  idx.extensions = [
+    "sugatoray.vscode-git-extension-pack"
+    "rust-lang.rust-analyzer"
+    "serayuzgur.crates"
+    "vadimcn.vscode-lldb"
+    "tamasfe.even-better-toml"
+  ];
+
+  # Enable previews and customize configuration
+  idx.previews = {
+    enable = true;
+   };
+}
