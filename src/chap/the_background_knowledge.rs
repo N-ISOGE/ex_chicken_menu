97e2ee2c4bcb526be799f2eccd6a638687ba009a
daseeni <daseeni@gmail.com>
2024-02-13 14:09:32 +0000

    개요 작성

---
+++ src/chap/the_background_knowledge.rs
@@ -0,0 +1,11 @@
+/// 
+/// # 배경지식
+/// 
+pub fn main() {
+    println!("시작 -> 배경지식 the_backgroud_knowledge");
+    // - [`type`]
+    // - [`function, statements, expressions`]
+    // - [`comment`]
+    // - [`control flow`]
+    println!("배경지식 the_backgroud_knowledge -> 변수와 가변성 the_mutability_shadowing ")
+} 
