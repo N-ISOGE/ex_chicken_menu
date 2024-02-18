6efb0f485be1031a134589762104891b966942a3
daseeni <daseeni@gmail.com>
2024-02-18 12:53:46 +0000

    구조체 예시 파일 작성, 모듈 최상위 main() 수정

---
+++ src/chap/the_example_of_using_structs.rs
@@ -0,0 +1,10 @@
+//!
+//! # 구조체를 사용한 예시
+//! 
+
+#[allow(unused)]
+pub fn main(){
+    println!("구조체란? the_defining_structs -> 구조체를 사용한 예시 the_example_of_using_structs");
+    
+    println!("구조체를 사용한 예시 the_example_of_using_structs -> 메서드 문법 the_method_syntax");
+}
