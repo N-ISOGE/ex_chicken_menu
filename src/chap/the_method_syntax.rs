cee13ba76b933ca911a187c939546a903132216c
daseeni <daseeni@gmail.com>
2024-02-22 13:24:16 +0000

    clippy 경고 관련 annotation 추가

--- src/chap/the_method_syntax.rs
+++ src/chap/the_method_syntax.rs
@@ -84,6 +84,7 @@ fn auto_refer_and_derefer() {
     let a_ellipse = Ellipse { a: 50.0, b: 30.0 };
 
     // 동일한 연산
+    #[allow(clippy::needless_borrow)]
     println!("타원의 넓이: {:.4}", (&a_ellipse).area());
     println!(" 넓이 출력 : {:.4}", a_ellipse.area());
 }
