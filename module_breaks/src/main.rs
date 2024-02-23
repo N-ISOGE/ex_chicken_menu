128b4ec5cd7bc9224b9323db9ddff0d50d6b666a
sang hyeon, son <daseeni@gmail.com>
2024-02-23 23:56:00 +0900

    충돌 임시 구현 ## 새로 만듬 동영상에서 0.11.0 버전인데 지금은 0.13.0 버전으로 차이남. 동영상에서 쓴 collide 함수가 없어지고 대신 math::bounding 모듈을 사용해야 함. ## math::bounding 사용 공식 홈페이지에서 0.13.0으로 바뀌면서 어떻게 해야할 지 예시가 나와있었음 [링크](https://bevyengine.org/learn/migration-guides/0-12-to-0-13/)

--- module_breaks/src/main.rs
+++ module_breaks/src/main.rs
@@ -1,12 +1,18 @@
-use bevy::{math, sprite, prelude::*};
-
+use bevy::{math, prelude::*};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, (move_paddle, apply_velocity))
+        .add_systems(
+            FixedUpdate,
+            (
+                move_paddle,
+                apply_velocity,
+                check_ball_collisions.after(apply_velocity),
+            ),
+        )
         .run();
 }
 
@@ -14,12 +20,13 @@ fn main() {
 
 // PADDLE
 const PADDLE_START_Y: f32 = 0.0;
-const PADDLE_SIZE: Vec2 = Vec2::new(120.0, 20.0);
+const PADDLE_SIZE: Vec3 = Vec3::new(120.0, 20., 0.);
 const PADDLE_COLOR: Color = Color::rgb(0.3, 0.2, 0.5);
 const PADDLE_SPEED: f32 = 700.0;
 
 // BALL
-const BALL_SIZE: Vec2 = Vec2::new(20.0, 20.0);
+const BALL_DIAMETER: f32 = 20.;
+const BALL_SIZE: Vec2 = Vec2::splat(BALL_DIAMETER);
 const BALL_COLOR: Color = Color::rgb(0.9, 0.1, 0.2);
 const BALL_SPEED: f32 = 500.0;
 const BALL_STARTING_POSITION: Vec3 = Vec3::new(0.0, -50.0, 1.);
@@ -36,22 +43,19 @@ const WALL_BLOCK_WIDTH: f32 = WALL_RIGHT - WALL_LEFT;
 const WALL_BLOCK_HEIGHT: f32 = WALL_TOP - WALL_BOTTOM;
 const WALL_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
 
-
 #[derive(Component)]
 struct Paddle;
 
 #[derive(Component)]
 struct Ball {
-    size: Vec2,
+    size: Circle,
 }
 
 #[derive(Component, Deref, DerefMut)]
 struct Velocity(Vec2);
 
 #[derive(Component)]
-struct Collider {
-    size: Vec2,
-}
+struct Collider;
 
 #[derive(Bundle)]
 struct WallBundle {
@@ -68,11 +72,11 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         SpriteBundle {
             transform: Transform {
                 translation: math::vec3(0., PADDLE_START_Y, 0.),
+                scale: PADDLE_SIZE,
                 ..default()
             },
             sprite: Sprite {
                 color: PADDLE_COLOR,
-                custom_size: Some(PADDLE_SIZE),
                 ..default()
             },
             ..default()
@@ -96,9 +100,84 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             texture: ball_tex,
             ..default()
         },
-        Ball,
+        Ball {
+            size: Circle::new(BALL_DIAMETER / 2.),
+        },
         Velocity(BALL_SPEED * BALL_INITIAL_DIRECTION),
     ));
+
+    // walls
+    {
+        let vertical_wall_size = math::vec2(WALL_THICKNESS, WALL_BLOCK_HEIGHT + WALL_THICKNESS);
+        let horizontal_wall_size = math::vec2(WALL_BLOCK_WIDTH + WALL_THICKNESS, WALL_THICKNESS);
+        // left wall
+        commands.spawn(WallBundle {
+            sprite_bundle: SpriteBundle {
+                transform: Transform {
+                    translation: math::vec3(WALL_LEFT, 0., 0.),
+                    scale: vertical_wall_size.extend(0.),
+                    ..default()
+                },
+                sprite: Sprite {
+                    color: WALL_COLOR,
+                    ..default()
+                },
+                ..default()
+            },
+            collider: Collider,
+        });
+
+        // right wall
+        commands.spawn(WallBundle {
+            sprite_bundle: SpriteBundle {
+                transform: Transform {
+                    translation: math::vec3(WALL_RIGHT, 0., 0.),
+                    scale: vertical_wall_size.extend(0.),
+                    ..default()
+                },
+                sprite: Sprite {
+                    color: WALL_COLOR,
+                    ..default()
+                },
+                ..default()
+            },
+            collider: Collider,
+        });
+
+        // bottom wall
+        commands.spawn(WallBundle {
+            sprite_bundle: SpriteBundle {
+                transform: Transform {
+                    translation: math::vec3(0., WALL_BOTTOM, 0.),
+                    scale: horizontal_wall_size.extend(0.),
+                    ..default()
+                },
+                sprite: Sprite {
+                    color: WALL_COLOR,
+                    ..default()
+                },
+                ..default()
+            },
+            collider: Collider,
+        });
+
+        // top wall
+        commands.spawn(WallBundle {
+            sprite_bundle: SpriteBundle {
+                transform: Transform {
+                    translation: math::vec3(0., WALL_TOP, 0.),
+                    scale: horizontal_wall_size.extend(0.),
+                    ..default()
+                },
+                sprite: Sprite {
+                    color: WALL_COLOR,
+                    ..default()
+                },
+                ..default()
+            },
+            collider: Collider,
+        });
+    }
 }
 
 fn move_paddle(
@@ -115,7 +194,6 @@ fn move_paddle(
     if input.pressed(KeyCode::KeyD) {
         direction += 1.0;
     }
-
     let new_x =
         paddle_transform.translation.x + direction * PADDLE_SPEED * time_step.delta_seconds();
 
@@ -130,5 +208,37 @@ fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time_step: Res<
     for (mut transform, velocity) in &mut query {
         transform.translation.x += velocity.x * delta_time;
         transform.translation.y += velocity.y * delta_time;
+    }
+}
+
+fn check_ball_collisions(
+    mut ball_query: Query<(&mut Velocity, &Transform), With<Ball>>,
+    collider_query: Query<(&Transform, &Collider)>,
+) {
+    let (mut ball_velocity, ball_transform) = ball_query.single_mut();
+
+    for (transform, other) in &collider_query {
+        use math::bounding::*;
+
+        let ball_bound =
+            BoundingCircle::new(ball_transform.translation.truncate(), BALL_DIAMETER / 2.);
+        let other_bound = Aabb2d::new(
+            transform.translation.truncate(),
+            transform.scale.truncate() / 2.,
+        );
+        let collision = ball_bound.intersects(&other_bound);
+
+        if collision {
+            let closest = other_bound.closest_point(ball_bound.center());
+            let offset = closest - ball_bound.center();
+
+            if offset.x.abs() > offset.y.abs() {
+                // x 축과 가까운 곳에서 충돌
+                ball_velocity.x = -ball_velocity.x;
+            } else {
+                // y 축과 가까운 곳에서 충돌
+                ball_velocity.y = -ball_velocity.y;
+            }
+        }
     }
 }
