dc5eefdead675cdd8b234af366efa174a6a597a2
sang hyeon, son <daseeni@gmail.com>
2024-07-15 16:18:32 +0900

    메크로 관련 예제 작성

--- example_in_the_book/src/rust_books/mod.rs
+++ example_in_the_book/src/rust_books/mod.rs
@@ -21,18 +21,19 @@
 //! 6. 열거형
 //!     - [`defining_enums`]
 //!
+pub mod defining_enums;
 pub mod number_game;
 pub mod the_background_knowledge;
 pub mod the_defining_structs;
 pub mod the_example_of_using_structs;
+pub mod the_marcos;
 pub mod the_method_syntax;
 pub mod the_mutability_shadowing;
 pub mod the_ownership;
 pub mod the_refer_borrow;
 pub mod the_slice;
-pub mod defining_enums;
 
 #[allow(dead_code)]
 pub fn main() {
-    defining_enums::main();
+    the_marcos::main();
 }
---
+++ example_in_the_book/src/rust_books/the_marcos.rs
@@ -0,0 +1,57 @@
+#[macro_export]
+macro_rules! my_assert_ne {
+    ($left:expr,$right:expr $(,)?) => {
+        match (&$left, &$right) {
+            (left_val, right_val) => {
+                if *left_val == *right_val {
+                    eprintln!(" ne {} {} !", *left_val, *right_val);
+                    // panic!("equal!");
+
+                    // let kind = $crate::panicking::AssertKind::Ne;
+                    // $crate::panicking::assert_failed(
+                    //     kind,
+                    //     &*left_val,
+                    //     &*right_val,
+                    //     $crate::option::Option::None,
+                    // );
+                }
+            }
+        }
+    };
+    ($left:expr,$right:expr, $($arg:tt)+) => {
+        match (&$left, &$right) {
+            (left_val, right_val) => {
+                if *left_val == *right_val {
+                    eprintln!("ne {} {}",*left_val,*right_val);
+
+                    eprintln!("{}",std::format_args!($($arg)+));
+
+                    // panic!("equal!");
+
+                    // let kind = $crate::panicking::AssertKind::Ne;
+                    // $crate::panicking::assert_failed(
+                    //     kind,
+                    //     &*left_val,
+                    //     &*right_val,
+                    //     $crate::option::Option::Some(
+                    //         $crate::formate_args!($($arg)+)
+                    //     ),
+                    // );
+                }
+            }
+        }
+    };
+}
+
+pub fn main() {
+    simple_example();
+}
+
+fn simple_example() {
+    let a = 3;
+    let b = 1 + 2;
+
+    assert_eq!(a, b);
+    my_assert_ne!(a, b, "추가 정보");
+    assert_eq!(a, b, "기본적으로 정수만 써서 계산된 결과 {}, {}", a, b);
+}
