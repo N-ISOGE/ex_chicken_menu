4f1da7651041573f688034f2fc24a384088157b9
sang hyeon, son <daseeni@gmail.com>
2024-07-24 17:59:51 +0900

    추가 팁 정리

--- example_in_the_book/src/ex_tips/mod.rs
+++ example_in_the_book/src/ex_tips/mod.rs
@@ -1,4 +1,6 @@
-/// 1. 초보가 범하기 쉬운 여러가지 실수
-///     - [`the_misstakes_by_newbies`]
-///
-pub mod the_misstakes_by_newbies;
+//! ## 추가 팁
+//!
+//! 1. 초보가 범하기 쉬운 여러가지 실수
+//!     - [`the_mistakes_by_newbies`]
+//!
+pub mod the_mistakes_by_newbies;
