d2b5e5f6008e66edf41b015331d308c1e0291fef
sang hyeon, son <daseeni@gmail.com>
2024-08-17 20:44:56 +0900

    Result 다루는 모듈 정리중

--- example_in_the_book/src/rust_books/mod.rs
+++ example_in_the_book/src/rust_books/mod.rs
@@ -59,6 +59,7 @@
 //!
 //! ### 에러 처리 상세 목록
 //! - [`the_panic_handling_error`]
+//! - [`the_result_handing_error`]
 //!
 pub mod number_game;
 pub mod the_background_knowledge;
@@ -76,6 +77,7 @@ pub mod the_ownership;
 pub mod the_package_and_crate;
 pub mod the_panic_handing_error;
 pub mod the_refer_borrow;
+pub mod the_result_handing_error;
 pub mod the_slice;
 pub mod the_strings;
 pub mod the_vector;
---
+++ example_in_the_book/src/rust_books/the_Result_handing_error.rs
@@ -0,0 +1,41 @@
+//! # Result, 복구 가능한 에러 처리
+//!
+//! ## 개요
+//!
+//! 에러가 났지만 대응할 여지가 있다면 중단하지 않을 수 있다.
+//! - 파일이 없어서 에러가 났다면 종료하는 대신 파일을 만드는 것을 고려할 수 있다.
+//!
+//! Result 열거형 모양
+//! ```rust
+//! enum Result<T, E> {
+//!     Ok(T),
+//!     Err(E),
+//! }
+//! ```
+//!
+
+#[test]
+fn signature_of_result() {
+    enum Result<T, E> {
+        Ok(T),
+        Err(E),
+    }
+
+    use std::fmt;
+
+    impl fmt::Display for Result<i32, String> {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            match self {
+                Result::Ok(num) => {
+                    write!(f, "{}", num)
+                }
+                Result::Err(error_thing) => {
+                    write!(f, "wow error {}", error_thing)
+                }
+            }
+        }
+    }
+
+    debug_assert_eq!("1", format!("{}", Result::Ok(1)));
+    debug_assert_eq!("wow error wow", format!("{}", Result::Err(String::from("wow"))));
+}
