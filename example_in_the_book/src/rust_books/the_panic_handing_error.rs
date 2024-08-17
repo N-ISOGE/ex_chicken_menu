3719eaed226aebc6f249b12a6df64b159d406f35
sang hyeon, son <daseeni@gmail.com>
2024-08-17 19:56:41 +0900

    panic! 다루는 모듈 정리

--- example_in_the_book/src/rust_books/the_panic_handing_error.rs
+++ example_in_the_book/src/rust_books/the_panic_handing_error.rs
@@ -1,2 +1,37 @@
 //! # panic!, 복구 불가능한 에러 처리
 //!
+//! ## 개요
+//!
+//! 대처할 방법이 없으면 쓰는 메크로, `panic!`
+//! 패닉을 일으키는 방법: 코드가 패닉을 일으키는 동작을 함, `panic!` 사용하기
+//! 패닉이 일어나면? -> 실패 메시지 출력, unwind, 스택 청소, 종료
+//! 환경 변수를 정의해두면 호출 스택을 출력 가능
+//!
+//! 실제로 패닉 사용해 보기
+//! - [`example_of_panic`]
+//!
+//! ## 백트레이스 이용하기
+//!
+//! - [`test_use_panic_backtrace`]
+//!
+//! 백트레이스를 보려면 백트레이스 관련 환경변수 `RUST_BACKTRACE`을 설정하면 됨.
+//! 백트레이스: 어떤 지점에 도달하기까지 호출한 모든 함수의 목록
+//!
+//! `panic!`을 쓸지 말지는 나중에 다룸.
+//!
+
+use test_log::test;
+
+#[test]
+#[should_panic(expected = "crash")]
+fn example_of_panic() {
+    panic!("crash and burn");
+}
+
+#[test]
+#[should_panic(expected = "index out of bounds")]
+fn test_use_panic_backtrace() {
+    let v = vec![1, 2, 3];
+
+    v[99];
+}
