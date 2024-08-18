6668534d43e3c1ed312c95669cf4393b6e4c0891
sang hyeon, son <daseeni@gmail.com>
2024-08-18 19:49:01 +0900

    패닉을 쓸지 다르게 할지 결정할지에 대한 모듈 생성

--- example_in_the_book/src/rust_books/mod.rs
+++ example_in_the_book/src/rust_books/mod.rs
@@ -60,7 +60,7 @@
 //! ### 에러 처리 상세 목록
 //! - [`the_panic_handling_error`]
 //! - [`the_result_handing_error`]
-//!
+//! - [`the_panic_or_something_else`]
 pub mod number_game;
 pub mod the_background_knowledge;
 pub mod the_defining_enums;
@@ -81,3 +81,4 @@ pub mod the_result_handing_error;
 pub mod the_slice;
 pub mod the_strings;
 pub mod the_vector;
+pub mod the_panic_or_something_else;
