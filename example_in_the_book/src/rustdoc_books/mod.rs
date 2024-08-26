ea4a4e59e81571a63f5c8c38829d11b8ac2d0f50
sang hyeon, son <daseeni@gmail.com>
2024-08-26 18:42:19 +0900

    rustdoc 모듈 생성 및 목차 작성
    러스트 독이란 모듈 생성

---
+++ example_in_the_book/src/rustdoc_books/mod.rs
@@ -0,0 +1,23 @@
+//! # rustdoc book
+//!
+//! ## 개요
+//!
+//! - 러스트 독이란?
+//!     - what is rustdoc?
+//! - command-line arguments
+//! - how to read rustdoc output
+//! - how to write documentation
+//! - rustdoc-specific lints
+//! - scraped examples
+//! - Advanced features
+//! - unstable features
+//! - deprecated features
+//! - references
+//!
+//! ## 러스트 독이란?
+//!
+//! - [`what_is_rustdoc`]
+//!
+//!
+
+pub mod what_is_rustdoc;
